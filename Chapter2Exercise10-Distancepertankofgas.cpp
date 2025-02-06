9e9d7514c7ff30314dbe8c31c639fd52c60e017e
Morgan Thorne <mt34452@email.vccs.edu>
2025-02-06 17:05:45 -0500

    Allowed user to enter gallons and miles travelled.

--- Chapter2Exercise10-Distancepertankofgas.cpp
+++ Chapter2Exercise10-Distancepertankofgas.cpp
@@ -6,6 +6,9 @@ Requirements:
 A car holds 15 gallons of gasoline and can travel 375 miles before refueling.
 Write a program that calculates the number of miles per gallon the car gets.
 Display the result on the screen.
+
+Revisions:
+Changed code to allow user to enter the amount of gallons in car and miles travelled.
 */
 
 
@@ -14,11 +17,16 @@ using namespace std;
 
 int main()
 {
-    int Gallons = 15;
-    int MilesDriven = 375;
-    int MPG = MilesDriven / Gallons;
+    int Gallons;
+    int MilesDriven;
 
-    cout << "Your car has " << Gallons << " gallons of petrol and can travel " << MilesDriven << " miles." << endl;
+    cout << "How many gallons of petrol does your car have? ";
+    cin >> Gallons;
+    cout << "How many miles can you drive before refueling? ";
+    cin >> MilesDriven;
+    
+    int MPG = MilesDriven / Gallons;
+    
     cout << "You can drive " << MPG << " miles per gallon of petrol.";
     
     
